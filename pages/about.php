d18f2b98cff003d1b66dfc1855f99cadade91ccf
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-04 08:13:15 -0300

    Add change to content of about page depending on the current language

--- pages/about.php
+++ pages/about.php
@@ -1,2 +1,9 @@
-<h1>Sobre</h1>
-<p>A fazer.</p>
+<?php
+
+const ABOUT_PATH_TEMPLATE = __DIR__.'/translations/about-%s.php';
+$about_path = sprintf(ABOUT_PATH_TEMPLATE, $lang_codes->get_lang_code());
+if (!file_exists($about_path))
+  $about_path = sprintf(ABOUT_PATH_TEMPLATE, LanguageText::DEFAULT_LANG_CODE);
+require($about_path);
+
+?>
