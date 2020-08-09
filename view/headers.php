21289048354a3acf1ee52bca522b06a7b5705c5a
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-09 10:14:00 -0300

    Add favicon

--- view/headers.php
+++ view/headers.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/public/css/style.css">
+<link rel="icon" href="/public/images/favicons/favicon-48x48.png">
 <script src="/public/js/script.js"></script>
 <title><?=$lang_codes->translate('Text Converter')?></title>
